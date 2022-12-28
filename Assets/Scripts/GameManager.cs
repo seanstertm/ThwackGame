6493a927053eb7f245ff3183480b2bb3852be52d
seanstertm <sean.patrick.richards@gmail.com>
2022-12-28 00:12:58 -0600

    improving menus

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 namespace UserCode
 {
+    public enum MenuState
+    {
+        home,
+        game,
+        paint
+    }
     public class GameManager : MonoBehaviour
     {
         public bool game { get; private set; } = false;
+        public MenuState menuState = MenuState.home;
         public int paddleArmWidth = 1;
         public int foresight = 1;
         public int numBalls = 1;
@@ -17,8 +24,24 @@ namespace UserCode
         [SerializeField] private GameObject launcher;
         [SerializeField] private GameObject menu;
 
+        private static GameManager mainReference;
+
+        public void Start()
+        {
+            mainReference = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();
+        }
+
+        public static GameManager Main 
+        {
+            get
+            {
+                return mainReference;
+            }
+        }
+
         public void BeginGame()
         {
+            menuState = MenuState.game;
             game = true;
             borders.SetActive(true);
             menu.SetActive(false);
--- Assets/Scripts/MenuInput.cs
+++ Assets/Scripts/MenuInput.cs
@@ -8,44 +8,70 @@ public enum ButtonType
     paint
 }
 
-public class MenuInput : MonoBehaviour
-{
-    [SerializeField] private List<GameObject> buttons;
-    private readonly List<(Vector3 topRight, Vector3 bottomLeft, ButtonType)> buttonInfo = new();
-
-    private void Start()
+namespace UserCode {
+    public class MenuInput : MonoBehaviour
     {
-        foreach(GameObject button in buttons)
+        [SerializeField] private List<GameObject> buttons;
+        private readonly List<(Vector3 topRight, Vector3 bottomLeft, ButtonType)> buttonInfo = new();
+        private bool pressed = false;
+
+        private void Start()
         {
-            (Vector3 topRight, Vector3 bottomLeft) = GetBounds(button);
-            ButtonType buttonType = button.GetComponent<ButtonID>().buttonType;
-            buttonInfo.Add((topRight, bottomLeft, buttonType));
-        }
-
-    }
-
-    private (Vector3, Vector3) GetBounds(GameObject button)
-    {
-        return (new Vector3(button.transform.position.x + 0.5f * button.transform.localScale.x, button.transform.position.y + 0.5f * button.transform.localScale.y, 0), new Vector3(button.transform.position.x - 0.5f * button.transform.localScale.x, button.transform.position.y - 0.5f * button.transform.localScale.y, 0));
-    }
-
-    private void Update()
-    {
-        if(Input.touches.Length != 0)
-        {
-            ButtonType pressedButton = GetButton(Camera.main.ScreenToWorldPoint(Input.touches[0].position));
-        }
-    }
-
-    private ButtonType GetButton(Vector3 worldPoint)
-    {
-        foreach((Vector3 topRight, Vector3 bottomLeft, ButtonType buttonType) in buttonInfo)
-        {
-            if(worldPoint.x > bottomLeft.x && worldPoint.x < topRight.x && worldPoint.y > bottomLeft.y && worldPoint.y < topRight.y)
+            foreach (GameObject button in buttons)
             {
-                return buttonType;
+                (Vector3 topRight, Vector3 bottomLeft) = GetBounds(button);
+                ButtonType buttonType = button.GetComponent<ButtonID>().buttonType;
+                buttonInfo.Add((topRight, bottomLeft, buttonType));
+            }
+
+        }
+
+        private (Vector3, Vector3) GetBounds(GameObject button)
+        {
+            return (new Vector3(button.transform.position.x + 0.5f * button.transform.localScale.x, button.transform.position.y + 0.5f * button.transform.localScale.y, 0),
+                    new Vector3(button.transform.position.x - 0.5f * button.transform.localScale.x, button.transform.position.y - 0.5f * button.transform.localScale.y, 0));
+        }
+
+        private void Update()
+        {
+            if (GameManager.Main.menuState != MenuState.home) { return; }
+
+            if (Input.touches.Length != 0)
+            {
+                if (!pressed)
+                {
+                    SwitchCallback(GetButton(Camera.main.ScreenToWorldPoint(Input.touches[0].position)));
+                    pressed = true;
+                }
+            }
+            else
+            {
+                pressed = false;
             }
         }
-        return ButtonType.game;
+
+        private ButtonType GetButton(Vector3 worldPoint)
+        {
+            foreach ((Vector3 topRight, Vector3 bottomLeft, ButtonType buttonType) in buttonInfo)
+            {
+                if (worldPoint.x > bottomLeft.x && worldPoint.x < topRight.x && worldPoint.y > bottomLeft.y && worldPoint.y < topRight.y)
+                {
+                    return buttonType;
+                }
+            }
+            return ButtonType.game;
+        }
+
+        private void SwitchCallback(ButtonType buttonType)
+        {
+            switch (buttonType)
+            {
+                case ButtonType.game:
+                    GameManager.Main.BeginGame();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
